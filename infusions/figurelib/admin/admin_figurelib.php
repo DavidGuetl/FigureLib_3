82e9c2646e7fe5e71cd8fec1bbcabc0377abeeb8
Catzenjaeger <catzenjaeger@gmail.com>
2016-06-22 23:16:14 +0200

    Update admin_figurelib.php

--- infusions/figurelib/admin/admin_figurelib.php
+++ infusions/figurelib/admin/admin_figurelib.php
@@ -1,5 +1,25 @@
 <?php
-
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: admin_figurelib.php based on admin/weblink.php
+| Author: PHP-Fusion Development Team
+|
+| Modification: Catzenjaeger
+| URL: www.aliencollectors.com
+| E-Mail: admin@aliencollectors.com
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) { die("Access Denied"); }
 if (!defined("IN_FUSION")) { die("Access Denied"); }
 pageAccess("FI");
 if (fusion_get_settings("tinymce_enabled")) {
