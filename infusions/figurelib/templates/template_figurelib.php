61631366cc52810e238e871caae4d240357e333b
Catzenjaeger <catzenjaeger@gmail.com>
2016-06-24 12:40:25 +0200

    Update template_figurelib.php

--- infusions/figurelib/templates/template_figurelib.php
+++ infusions/figurelib/templates/template_figurelib.php
@@ -21,6 +21,9 @@
 if (!defined("IN_FUSION")) { die("Access Denied"); }
 include INFUSIONS."figurelib/infusion_db.php";
 
+// SETTINGS HOLEN
+$fil_settings = get_settings("figurelib");
+
 
 if (!function_exists('render_figure_item')) {
 	function render_figure_item($info) {
@@ -101,14 +104,11 @@ if (!function_exists('render_figure_item')) {
 	
 		} else {
 			
-					echo "<div class='panel panel-default'>\n";
+		echo "<div class='panel panel-default'>\n";
+			
 			echo "<div class='panel-heading'>\n";
-				echo "<a href='".$data['figure']['link']."'>".$data['figure']['name']."</a>\n";
-		echo "</div>\n";
-				
-		//echo "<div class='overflow-hide m-b-20'>\n";
-		//echo "<h2 class='figure_title'>".$data['figure_title']."</span>\n</h2>\n";
-		//echo "</div>\n";
+			echo "<a href='".$data['figure']['link']."'>".$data['figure']['name']."</a>\n";
+			echo "</div>\n";
 	   
 		echo "<div class='list-group-item m-b-20'>\n";
 		echo "<div class='row'>\n";
@@ -165,23 +165,57 @@ if (!function_exists('render_figure_item')) {
 				$rating = ($drating['count_votes'] > 0 ? str_repeat("<img src='".INFUSIONS.$inf_folder."/images/starsmall.png'>",ceil($drating['sum_rating']/$drating['count_votes'])) : "-");
 			echo "<span class='small'><strong>Rating: </strong>".$rating."</span>\n";
 
+		echo "</div>\n";
+		
+		
 		echo "<div class='clearfix'>\n";
 		echo "<div class='btn-group pull-right m-t-20'>\n";
 		echo isset($data['nav']['first']) ? "<a class='btn btn-default btn-sm' href='".$data['nav']['first']['link']."' title='".$data['nav']['first']['name']."'><i class='entypo to-start'></i></a>\n" : '';
 		echo isset($data['nav']['prev']) ? "<a class='btn btn-default btn-sm' href='".$data['nav']['prev']['link']."' title='".$data['nav']['prev']['name']."'><i class='entypo left-dir'></i></a>\n" : '';
 		echo isset($data['nav']['next']) ? "<a class='btn btn-default btn-sm' href='".$data['nav']['next']['link']."' title='".$data['nav']['next']['name']."'><i class='entypo right-dir'></i></a>\n" : '';
 		echo isset($data['nav']['last']) ? "<a class='btn btn-default btn-sm' href='".$data['nav']['last']['link']."' title='".$data['nav']['last']['name']."'><i class='entypo to-end'></i></a>\n" : '';
+		echo "</div>";
 		
 
-		echo "</div>\n</div>\n";
-			echo "</div>\n";
-		
+		echo "</div>\n";
+		echo "</div>\n";
+		echo "</div>\n";
+		echo "</div>\n";
+
 				}
-			}
+			}		
 
-		} else {
+} else {
 			// ['figc_0012'] = "No figure categories defined";
-			echo "<div style='text-align:center'><br />\n".$locale['figc_0012']."<br /><br />\n</div>\n";
+			
+			// SETTINGS HOLEN
+			$fil_settings = get_settings("figurelib");			
+			
+			
+/*			// graue Version der Box
+			echo "<div class='well text-center'><br />\n".$locale['figc_0012']."<br />\n";
+			if (iMEMBER && $fil_settings['figure_submit']) {
+			//['figure_521'] = "Submit Figure";
+					echo "<p><a href='submit.php?stype=f'>".$locale['figure_521']."</a></p>";
+				echo "</div>\n";
+			} else {
+				echo "</div>\n";
+			}
+			
+*/			
+			
+			// blaue Version der Box
+			echo "<div class='alert alert-info m-b-20 submission-guidelines'><br /><center>\n".$locale['figc_0012']."<br>";
+			
+			if (iMEMBER && $fil_settings['figure_submit']) {
+			//['figure_521'] = "Submit Figure";
+					echo "<p><a href='submit.php?stype=f'>".$locale['figure_521']."</a></p>";
+				echo "</div>\n";
+			} else {
+				echo "</div>\n";
+			}
+					
+			
 		}
 
 		closetable();
