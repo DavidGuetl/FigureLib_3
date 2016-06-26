318842b52c6f20cf1e69c62d3d79206313df2832
Catzenjaeger <catzenjaeger@gmail.com>
2016-06-26 19:21:41 +0200

    Update figures.php

--- infusions/figurelib/figures.php
+++ infusions/figurelib/figures.php
@@ -39,7 +39,11 @@ if (file_exists(INFUSIONS."figurelib/locale/".LOCALESET."locale_figurelib.php"))
 } else {
     include INFUSIONS."figurelib/locale/English/locale_figurelib.php";
 }
-include INFUSIONS."figurelib/templates/template_figurelib.php";
+
+include INFUSIONS."figurelib/templates/template_clickcounter.php"; // COUNTER FOR HOW MANY TIMES A FIGURE CLICKED
+include INFUSIONS."figurelib/templates/template_render_figure.php"; // TEMPLATE FOR FIGURE OVERVIEW PER CATEGORY
+include INFUSIONS."figurelib/templates/template_render_figure_cats.php"; // TEMPLATE FOR CATEGORIES OVERVIEW
+include INFUSIONS."figurelib/templates/template_render_figure_items.php"; // TEMPLATE FOR DETAILS OF A FIGURE
 
 // SETTINGS HOLEN
 $fil_settings = get_settings("figurelib");
@@ -48,20 +52,59 @@ $fil_settings = get_settings("figurelib");
 $figure_cat_index = dbquery_tree(DB_FIGURE_CATS, 'figure_cat_id', 'figure_cat_parent');
 add_breadcrumb(array('link' => INFUSIONS.'figurelib/figures.php', 'title' => \PHPFusion\SiteLinks::get_current_SiteLinks("", "link_name")));
 
+
+
 if (!isset($_GET['figure_id']) || !isset($_GET['figure_cat_id'])) {
 	set_title($locale['INF_TITLE']);
 }
-	
+		
+
+
+		
 if (isset($_GET['figure_id']) && isnum($_GET['figure_id'])) {
 	$res = 0;
-	$data = dbarray(dbquery("SELECT * ".DB_FIGURE_ITEMS." WHERE figure_id='".intval($_GET['figure_id'])."'"));
+		$data = dbarray(dbquery("SELECT * ".DB_FIGURE_ITEMS." WHERE figure_id='".intval($_GET['figure_id'])."'"));
 	if (checkgroup($data['figure_visibility'])) {
-		$res = 1;
+	$res = 1;
 		dbquery("UPDATE ".DB_FIGURE_ITEMS." SET figure_clickcount=figure_clickcount+1 WHERE figure_id='".intval($_GET['figure_id'])."'");
-        redirect($data['figure_title']);
+		
+//+++++++++++++++++++
+
+$result = dbquery(
+			"SELECT tb.figure_id, tb.figure_submitter, tb.figure_clickcount, tb.figure_freigabe, tb.figure_pubdate, tb.figure_scale, tb.figure_title, tb.figure_manufacturer, tb.figure_brand, tb.figure_datestamp, tb.figure_cat, tbc.figure_cat_id, tbc.figure_cat_name, tbu.user_id, tbu.user_name, tbu.user_status, tbu.user_avatar, tbm.figure_manufacturer_name, tbb.figure_brand_name, tby.figure_year_id, tby.figure_year, tbs.figure_scale_id, tbs.figure_scale_name FROM ".DB_FIGURE_ITEMS." tb
+			LEFT JOIN ".DB_USERS." tbu ON tb.figure_submitter=tbu.user_id
+			INNER JOIN ".DB_FIGURE_CATS." tbc ON tb.figure_cat=tbc.figure_cat_id
+			INNER JOIN ".DB_FIGURE_MANUFACTURERS." tbm ON tbm.figure_manufacturer_id = tb.figure_manufacturer
+			INNER JOIN ".DB_FIGURE_BRANDS." tbb ON tbb.figure_brand_id = tb.figure_brand
+			INNER JOIN ".DB_FIGURE_SCALES." tbs ON tbs.figure_scale_id = tb.figure_scale
+			INNER JOIN ".DB_FIGURE_YEARS." tby ON tby.figure_year_id = tb.figure_pubdate
+			".(multilang_table("FI") ? "WHERE figure_language='".LANGUAGE."' AND" : "WHERE")." tb.figure_freigabe='1' ORDER BY figure_id ASC");	
+			
+			if (dbrows($result) != 0) {
+				while($data = dbarray($result)){
+					$data['new'] = ($data['figure_datestamp']+604800 > time()+($settings['timeoffset']*3600)) ? 1 : 0;
+					$data['figure'] = array(
+						'link' => INFUSIONS."figurelib/figures.php?figure_id=".$data['figure_id'],
+						'name' => $data['figure_title'],
+						'manufacturer' => $data['figure_manufacturer_name'],
+						'scale' => $data['figure_scale_name'],
+						'year' => $data['figure_year'],
+						'brand' => $data['figure_brand_name'],
+						'series' => $data['figure_series'],
+						'variant' => $data['figure_variant'],
+						'views' => $data['figure_clickcount']
+					);
+					$info['item'][$data['figure_id']] = $data;
+				}
+			}
+
+        render_figure_items($info);
 	} else {
-        redirect(FUSION_SELF);
-    }
+		redirect(FUSION_SELF);
+	}
+//+++++++++++++++++++		
+		
+
 	
 } elseif (isset($_GET['cat_id']) && isnum($_GET['cat_id'])) {	
 	$info = array();
@@ -97,6 +140,7 @@ if (isset($_GET['figure_id']) && isnum($_GET['figure_id'])) {
 							tb.figure_manufacturer, 
 							tb.figure_brand, 
 							tb.figure_datestamp, 
+							tb.figure_clickcount,
 							tb.figure_cat, 
 							tbc.figure_cat_id, 
 							tbc.figure_cat_name, 
@@ -132,7 +176,7 @@ if (isset($_GET['figure_id']) && isnum($_GET['figure_id'])) {
 				while ($data = dbarray($result)) {
 					$data['new'] = ($data['figure_datestamp']+604800 > time()+($settings['timeoffset']*3600)) ? 1 : 0;
 					$data['figure'] = array(
-						'link' => INFUSIONS."figurelib/figures.php?figure_id=".$_GET['figure_id']."&amp;figure_id=".$data['figure_id'],
+						'link' => INFUSIONS."figurelib/figures.php?figure_id=".$data['figure_id'],
 						'name' => $data['figure_title'],
 						'manufacturer' => $data['figure_manufacturer_name'],
 						'scale' => $data['figure_scale_name'],
@@ -146,7 +190,7 @@ if (isset($_GET['figure_id']) && isnum($_GET['figure_id'])) {
 				}
 			}
 		}
-        render_figure_item($info);
+        render_figure($info);
 	} else {
 		redirect(FUSION_SELF);
 	}
@@ -184,9 +228,13 @@ if (isset($_GET['figure_id']) && isnum($_GET['figure_id'])) {
 			$info['item'][$data['figure_cat_id']] = $data;
 		}
 	}
-	render_figure_cat($info);
+	render_figure_cats($info);
 }
 require_once THEMES."templates/footer.php";
+
+
+
+
 
 /**
  * FigureLib Category Breadcrumbs Generator
