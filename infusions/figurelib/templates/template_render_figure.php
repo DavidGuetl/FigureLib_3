f5f8495bcab1c71494877d020d11265283a4658d
Catzenjaeger <catzenjaeger@gmail.com>
2016-06-29 08:33:16 +0200

    Update template_render_figure.php

--- infusions/figurelib/templates/template_render_figure.php
+++ infusions/figurelib/templates/template_render_figure.php
@@ -1,10 +1,10 @@
 <?php
 
-	error_reporting(E_ALL);
+	//error_reporting(E_ALL);
 	// Formularinhalte prüfen
-	print_r ($_POST);
+	//print_r ($_POST);
 	// GET-Parameter prüfen
-	print_r ($_GET);
+	//print_r ($_GET);
 	// Sessions prüfen
 	//print_r ($_SESSION);
 
@@ -49,7 +49,7 @@ include INFUSIONS."figurelib/infusion_db.php";
 					
 					// ['cifg_0009'] = "Filter by:";
 					//opentable($locale['cifg_0009']);			
-					opentable();	
+					openside();	
 					
 					if ($info['figure_rows'] != 0) {						
 						$counter = 0;
@@ -92,7 +92,7 @@ include INFUSIONS."figurelib/infusion_db.php";
 							WHERE rating_type='FI' 
 							AND  rating_item_id='".$data['figure_id']."'
 						")); 
-				$rating = ($drating['count_votes'] > 0 ? str_repeat("<img src='".INFUSIONS.$inf_folder."/images/starsmall.png'>",ceil($drating['sum_rating']/$drating['count_votes'])) : "-");
+				$rating = ($drating['count_votes'] > 0 ? str_repeat("<img src='".INFUSIONS."figurelib/images/starsmall.png'>",ceil($drating['sum_rating']/$drating['count_votes'])) : "-");
 			echo "<span class='small'><strong>Rating: </strong>".$rating."</span>\n";
 	
 
@@ -159,7 +159,7 @@ include INFUSIONS."figurelib/infusion_db.php";
 					
 					// ['cifg_0009'] = "Filter by:";
 					//opentable($locale['cifg_0009']);
-					opentable();
+					openside();
 					
 					if ($info['figure_rows'] != 0) {
 						$counter = 0;
@@ -204,12 +204,13 @@ include INFUSIONS."figurelib/infusion_db.php";
 	echo "</div><div class='col-xs-12 col-sm-6 col-md-6 col-lg-6'>\n";
 	
 			// LEFT SIDE
-		
-			echo "<span class='small'><strong>Submitted by: </strong>".profile_link($data['user_id'], $data['user_name'], $data['user_status'])."<br/></span>\n";
+			
+			echo "<span class='small'><strong>Submitted by: </strong>".profile_link($data['figure']['userid'], $data['figure']['username'], $data['figure']['userstatus'])."<br/></span>\n";
+			
 			echo "<span class='small'><strong>Submitted on: </strong>".timer($data['figure_datestamp'])." - ".showdate("shortdate", $data['figure_datestamp'])."<br/></span>\n";
 			echo "<span class='small'><strong>Views: </strong>".$data['figure']['views']."<br/></span>\n";
 			echo "<span class='small'><strong>User Count: </strong>Variable  hier einbauen<br/></span>\n";
-			
+
 			$comments = dbcount("(comment_id)", DB_COMMENTS, "comment_type='FI' AND comment_item_id='".$data['figure_id']."'");
 			echo "<span class='small'><strong>Comments: </strong>".$comments."<br/></span>\n";
 			// Bewertung
@@ -221,22 +222,10 @@ include INFUSIONS."figurelib/infusion_db.php";
 							WHERE rating_type='FI' 
 							AND  rating_item_id='".$data['figure_id']."'
 						")); 
-				$rating = ($drating['count_votes'] > 0 ? str_repeat("<img src='".INFUSIONS.$inf_folder."/images/starsmall.png'>",ceil($drating['sum_rating']/$drating['count_votes'])) : "-");
+				$rating = ($drating['count_votes'] > 0 ? str_repeat("<img src='".INFUSIONS."figurelib/images/starsmall.png'>",ceil($drating['sum_rating']/$drating['count_votes'])) : "-");
 			echo "<span class='small'><strong>Rating: </strong>".$rating."</span>\n";
 
-		echo "</div>\n";
-		
-		
-		echo "<div class='clearfix'>\n";
-		echo "<div class='btn-group pull-right m-t-20'>\n";
-		echo isset($data['nav']['first']) ? "<a class='btn btn-default btn-sm' href='".$data['nav']['first']['link']."' title='".$data['nav']['first']['name']."'><i class='entypo to-start'></i></a>\n" : '';
-		echo isset($data['nav']['prev']) ? "<a class='btn btn-default btn-sm' href='".$data['nav']['prev']['link']."' title='".$data['nav']['prev']['name']."'><i class='entypo left-dir'></i></a>\n" : '';
-		echo isset($data['nav']['next']) ? "<a class='btn btn-default btn-sm' href='".$data['nav']['next']['link']."' title='".$data['nav']['next']['name']."'><i class='entypo right-dir'></i></a>\n" : '';
-		echo isset($data['nav']['last']) ? "<a class='btn btn-default btn-sm' href='".$data['nav']['last']['link']."' title='".$data['nav']['last']['name']."'><i class='entypo to-end'></i></a>\n" : '';
-		echo "</div>";
-		
-
-		echo "</div>\n";
+		echo "</div>\n";		
 		echo "</div>\n";
 		echo "</div>\n";
 		echo "</div>\n";
@@ -289,7 +278,7 @@ include INFUSIONS."figurelib/infusion_db.php";
 			
 		}
 		echo $info['page_nav'] ? "<div class='text-right'>".$info['page_nav']."</div>" : '';
-		closetable();
+		closeside();
 	}
 }					
 						
